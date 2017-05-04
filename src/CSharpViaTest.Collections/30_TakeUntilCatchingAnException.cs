c79ceaa19b04cd5b677a7a2f9309dc712398aee8
Liu Xia <lxconan@gmail.com>
2017-05-04 11:00:55 +0800

    [liuxia] add TakeUntilCatchingAnException test.

---
+++ src/CSharpViaTest.Collections/30_TakeUntilCatchingAnException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CSharpViaTest.Collections
+{
+    public class TakeUntilCatchingAnException
+    {
+        readonly int indexThatWillThrow = new Random().Next(2, 10);
+
+        IEnumerable<int> GetSequenceOfData()
+        {
+            for (int i = 0;; ++i)
+            {
+                if (i == indexThatWillThrow) { throw new Exception("An exception is thrown"); }
+                yield return i;
+            }
+        }
+
+        #region Please modifies the code to pass the test 
+
+        static IEnumerable<int> TakeUntilError(IEnumerable<int> sequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        [Fact]
+        public void should_get_sequence_until_an_exception_is_thrown()
+        {
+            IEnumerable<int> sequence = TakeUntilError(GetSequenceOfData());
+            Assert.Equal(Enumerable.Range(0, indexThatWillThrow + 1), sequence);
+        }
+    }
+}

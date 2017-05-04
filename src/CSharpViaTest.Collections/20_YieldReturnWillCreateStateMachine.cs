e01ffa234c5e91a861828bf40229e513b353c8d3
Liu Xia <lxconan@gmail.com>
2017-05-04 10:41:19 +0800

    [liuxia] Add yield return practice.

---
+++ src/CSharpViaTest.Collections/20_YieldReturnWillCreateStateMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CSharpViaTest.Collections
+{
+    public class YieldReturnWillCreateStateMachine
+    {
+        #region Please modifies the code to pass the test 
+
+        public IEnumerable<string> GetStringTriangle(char character, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        [Fact]
+        public void should_get_skipped_sequences()
+        {
+            IEnumerable<string> enumerable = GetStringTriangle('*', 4);
+            string[] expected =
+            {
+                "*",
+                "**",
+                "***",
+                "****"
+            };
+
+            Assert.Equal(expected, enumerable);
+        }
+
+        [Fact]
+        public void should_returns_enumerable_rather_than_collection()
+        {
+            IEnumerable<string> enumerable = GetStringTriangle('*', 2);
+            Assert.False(enumerable is ICollection<string>);
+        }
+    }
+}

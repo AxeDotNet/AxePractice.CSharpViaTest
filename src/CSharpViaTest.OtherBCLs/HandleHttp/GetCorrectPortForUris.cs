3591c319f6291754e97f9aa94a2588496f83f572
Liu Xia <lxconan@gmail.com>
2017-06-17 23:18:47 +0800

    [liuxia] Adjust namespaces.

--- src/CSharpViaTest.OtherBCLs/HandleHttp/GetCorrectPortForUris.cs
+++ src/CSharpViaTest.OtherBCLs/HandleHttp/GetCorrectPortForUris.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Xunit;
 
-namespace CSharpViaTest.OtherBCLs.HandleUris
+namespace CSharpViaTest.OtherBCLs.HandleHttp
 {
     /* 
      * Description
--- src/CSharpViaTest.OtherBCLs/HandleHttp/GetQueryParameters.cs
+++ src/CSharpViaTest.OtherBCLs/HandleHttp/GetQueryParameters.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace CSharpViaTest.OtherBCLs.HandleUris
+namespace CSharpViaTest.OtherBCLs.HandleHttp
 {
     /* 
      * Description
